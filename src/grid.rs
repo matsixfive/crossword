42cb73b587a74d1b129d0237540cc597be70c11a
matsixfive <maffers2006@gmail.com>
2023-04-27 17:42:53 +0100

    border

--- src/grid.rs
+++ src/grid.rs
@@ -1,3 +1,4 @@
+use crate::{Error, Result};
 use std::{cmp, fmt::Display};
 
 // mod word;
@@ -6,16 +7,18 @@ use crate::word::*;
 pub struct Grid {
     pub width: usize,
     pub height: usize,
-    // pub letters: Vec<char>,
+    pub letters: Vec<char>,
     pub words: Vec<Word>,
 }
 
 impl Grid {
     pub fn new(width: usize, height: usize) -> Grid {
+        let letters = vec![' '; width * height];
+
         Grid {
             width,
             height,
-            // letters: Vec::from([' ']),
+            letters,
             words: Vec::new(),
         }
     }
@@ -48,24 +51,91 @@ impl Grid {
         all_combinations
     }
 
-    pub fn add_word(&mut self, word: Word) {
-        self.words.push(word);
+    pub fn add_word(&mut self, word: Word) -> Result<()> {
+        if self.test_word(&word) {
+            let word_chars = &word.word.chars().collect::<Vec<char>>();
+            for i in 0..word_chars.len() {
+                match word.direction {
+                    Direction::North => {
+                        self.letters[(word.y - i) * self.height + word.x] = word_chars[i];
+                    }
+                    Direction::NorthEast => {
+                        self.letters[(word.y - i) * self.height + word.x + i] = word_chars[i];
+                    }
+                    Direction::East => {
+                        self.letters[word.y * self.height + word.x + i] = word_chars[i];
+                    }
+                    Direction::SouthEast => {
+                        self.letters[(word.y + i) * self.height + word.x + i] = word_chars[i];
+                    }
+                    Direction::South => {
+                        self.letters[(word.y + i) * self.height + word.x] = word_chars[i];
+                    }
+                    Direction::SouthWest => {
+                        self.letters[(word.y + i) * self.height + word.x - i] = word_chars[i];
+                    }
+                    Direction::West => {
+                        self.letters[word.y * self.height + word.x - i] = word_chars[i];
+                    }
+                    Direction::NorthWest => {
+                        self.letters[(word.y - i) * self.height + word.x - i] = word_chars[i];
+                    }
+                }
+            }
+            self.words.push(word);
+            return Ok(());
+        } else {
+            return Err(Error::from("word does not fit"));
+        }
     }
 
-    pub fn test_word(&self, word: &str) -> bool {
-        let word_chars = word.chars().collect::<Vec<char>>();
+    pub fn test_word(&self, word: &Word) -> bool {
+        let word_chars = word.word.chars().collect::<Vec<char>>();
 
-        // horizontal
+        match word.direction {
+            Direction::North => {
+                todo!()
+            }
+            Direction::NorthEast => {
+                todo!()
+            }
+            Direction::East => {
+                for (char_index, char) in word_chars.iter().enumerate() {
+                    if self.letters[word.y * self.height + word.x + char_index] == ' ' {
+                        continue;
+                    }
 
-        todo!()
+                    if self.letters[word.y * self.height + word.x + char_index] != *char {
+                        return false;
+                    }
+                }
+            }
+            Direction::SouthEast => todo!(),
+            Direction::South => todo!(),
+            Direction::SouthWest => todo!(),
+            Direction::West => {
+                for (char_index, char) in word_chars.iter().rev().enumerate() {
+                    if self.letters[word.y * self.height + word.x + char_index] == ' ' {
+                        continue;
+                    }
+
+                    if self.letters[word.y * self.height + word.x + char_index] != *char {
+                        return false;
+                    }
+                }
+            }
+            Direction::NorthWest => todo!(),
+        }
+
+        true
     }
 }
 
 impl Display for Grid {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut charcters: Vec<Vec<char>> = Vec::new();
+        let mut characters: Vec<Vec<char>> = Vec::new();
         for _ in 0..self.height {
-            charcters.push(vec!['a'; self.width]);
+            characters.push(vec!['a'; self.width]);
         }
 
         for word in &self.words {
@@ -74,14 +144,14 @@ impl Display for Grid {
                 let x = word.x;
                 let y = word.y;
                 match word.direction {
-                    Direction::North => charcters[y - i][x] = word_chars[i],
-                    Direction::NorthEast => charcters[y - i][x + i] = word_chars[i],
-                    Direction::East => charcters[y][x + i] = word_chars[i],
-                    Direction::SouthEast => charcters[y + i][x + i] = word_chars[i],
-                    Direction::South => charcters[y + i][x] = word_chars[i],
-                    Direction::SouthWest => charcters[y + i][x - i] = word_chars[i],
-                    Direction::West => charcters[y][x - i] = word_chars[i],
-                    Direction::NorthWest => charcters[y - i][x - i] = word_chars[i],
+                    Direction::North => characters[y - i][x] = word_chars[i],
+                    Direction::NorthEast => characters[y - i][x + i] = word_chars[i],
+                    Direction::East => characters[y][x + i] = word_chars[i],
+                    Direction::SouthEast => characters[y + i][x + i] = word_chars[i],
+                    Direction::South => characters[y + i][x] = word_chars[i],
+                    Direction::SouthWest => characters[y + i][x - i] = word_chars[i],
+                    Direction::West => characters[y][x - i] = word_chars[i],
+                    Direction::NorthWest => characters[y - i][x - i] = word_chars[i],
                 }
             }
         }
@@ -106,21 +176,36 @@ impl Display for Grid {
                     .collect::<Vec<&str>>(),
             ));
         }
-        println!("{:?}", word_columns);
 
-        for (line_num, line) in charcters.iter().enumerate() {
+
+        let mut top_str = String::new();
+        top_str.push('┏');
+        // top_str.push('╭');
+        for _ in 0..(self.width * 2 + 1) {
+            // top_str.push('─');
+            top_str.push('━');
+        }
+        top_str.push('┱');
+        // top_str.push('╮');
+        println!("{}", top_str);
+
+        for (line_num, line) in characters.iter().enumerate() {
             let mut line_string = String::new();
+
+            line_string.push_str("┃ ");
+            // line_string.push_str("│ ");
+
             for c in line {
                 // write!(f, "{} ", c)?;
                 line_string.push(*c);
                 line_string.push(' ');
             }
 
-
             // write words
             for i in 0..word_columns_amount {
                 if i == 0 {
-                    line_string.push_str("  ");                
+                    line_string.push_str("┃  ");
+                    // line_string.push_str("│  ");
                 }
 
                 if word_columns[i].len() > line_num {
@@ -138,6 +223,19 @@ impl Display for Grid {
 
             writeln!(f, "{}", line_string);
         }
+
+        let mut bot_str = String::new();
+        bot_str.push('┗');
+        // bot_str.push('╰');
+        for _ in 0..(self.width * 2 + 1) {
+            bot_str.push('━');
+            // bot_str.push('─');
+        }
+        // bot_str.push('┛');
+        bot_str.push('┹');
+        // bot_str.push('╯');
+        println!("{}", bot_str);
+
         write!(f, "")
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,25 @@ mod grid;
 mod word;
 use crate::grid::*;
 
+pub type Error = Box<dyn std::error::Error>;
+pub type Result<T> = std::result::Result<T, Error>;
+
 fn main() {
-    let mut grid = Grid::new(20, 4);
+    let mut grid = Grid::new(20, 20);
     println!("{}", grid.num_of_combinations("hello".len()));
     grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
-    grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
-    grid.add_word(word::Word { word: "yea".to_string(), x: 3, y: 2, direction: word::Direction::East });
-    grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
-    grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
-    grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
-    grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
-    grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
-    grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
-    grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
-    grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
-    grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
-    grid.add_word(word::Word { word: "yap".to_string(), x: 4, y: 1, direction: word::Direction::South });
+    // grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
+    grid.add_word(word::Word { word: "yea".to_string(), x: 3, y: 3, direction: word::Direction::East });
+    // grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
+    // grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
+    // grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
+    // grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
+    // grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
+    // grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
+    // grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
+    // grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
+    // grid.add_word(word::Word { word: "hello".to_string(), x: 3, y: 2, direction: word::Direction::East });
+    // grid.add_word(word::Word { word: "yap".to_string(), x: 4, y: 1, direction: word::Direction::South });
     println!("{}", grid);
 
 }
